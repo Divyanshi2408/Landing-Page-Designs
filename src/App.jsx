91ace73d80fde47dc8b841a43873ea375c01a82f
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-04-01 21:11:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TechnicalLandingPage from './Pages/TechnicalLandingPage'
+
+const App = () => {
+  return (
+    <div> 
+    <TechnicalLandingPage/>
+    </div>
+  )
+}
+
+export default App
